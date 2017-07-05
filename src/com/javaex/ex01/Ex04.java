8a2ff072e32b4bcbfe097201b59086c06d67b8ba
BIT <BIT@DESKTOP-DOGVD1T>
2017-07-05 16:50:43 +0900

    3,4번 완료

---
+++ src/com/javaex/ex01/Ex04.java
@@ -0,0 +1,20 @@
+package com.javaex.ex01;
+
+public class Ex04 {
+
+	public static void main(String[] args) {
+	
+		for(int i=1;i<11;i++) {
+			int num = i;
+			
+			for(int k=1;k<11;k++) {
+				System.out.print(num+"	");
+				num++;
+			}
+		
+			System.out.println();
+		}
+
+	}
+
+}
